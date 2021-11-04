3f2b5fca2caf365d67f51bb3a5aeb7801f4ed71b
ismayil <mohammedismayil10@gmail.com>
2021-11-04 23:45:05 +0530

    header works

--- src/Components/AutoAd.js
+++ src/Components/AutoAd.js
@@ -2,18 +2,27 @@ import React from "react";
 
 export default function AutoAd() {
   return (
-    <div className="relative">
-      <div className="">
-        <img
-          src="https://statics.olx.in/olxin/banners/hero_bg_in@1x.png"
-          alt=""
-          className="object-none  w-full "
-        ></img>
-      </div>
-
-      <div className="absolute justify-center items-center ">
-        Now, Buy and Sell Cars directly with OLX Autos
+    // <div className="">
+    <div className=" relative h-28 bg-black">
+      <img
+        src="https://statics.olx.in/olxin/banners/hero_bg_in@1x.png"
+        alt=""
+        className="object-none  w-full h-full"
+      ></img>
+      <div className="absolute top-1/3 left-1/4 pl-24 rounded-lg flex">
+        <div className="text-white w-1/3 text-xl">
+          Now, Buy and Sell Cars directly with OLX Autos
+        </div>
+        <div className="flex content-center justify-center ml-10">
+          <button className="bg-white rounded-md text-black font-semibold mx-4 py-2 px-4 h-3/4">
+            Buy Car
+          </button>
+          <button className="bg-white rounded-md text-black font-semibold mx-4 py-2 px-4 h-3/4">
+            Sell Car
+          </button>
+        </div>
       </div>
     </div>
+    // </div>
   );
 }
