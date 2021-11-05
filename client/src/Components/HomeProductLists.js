2eceddf1528536e4ce9b19e310dc03f0a33c7a2b
ismayil <mohammedismayil10@gmail.com>
2021-11-05 20:52:51 +0530

    checking the deployment time

--- client/src/Components/HomeProductLists.js
+++ client/src/Components/HomeProductLists.js
@@ -4,13 +4,13 @@ export default function HomeProductLists() {
   return (
     <div className="flex content-center justify-center mx-20">
       <div className="flex w-full bg-gray-600 flex-wrap m-auto content-center">
-        <div className=" bg-red-300 h-56 w-1/5 mx-5"></div>
-        <div className=" bg-red-300 h-56 w-1/5 mx-5"></div>
-        <div className=" bg-red-300 h-56 w-1/5 mx-5"></div>
-        <div className=" bg-red-300 h-56 w-1/5 mx-5"></div>
-        <div className=" bg-red-300 h-56 w-1/5 mx-5"></div>
-        <div className=" bg-red-300 h-56 w-1/5 mx-5"></div>
-        <div className=" bg-red-300 h-56 w-1/5 mx-5"></div>
+        <div className=" bg-red-600 h-56 w-1/5 mx-5"></div>
+        <div className=" bg-red-600 h-56 w-1/5 mx-5"></div>
+        <div className=" bg-red-600 h-56 w-1/5 mx-5"></div>
+        <div className=" bg-red-600 h-56 w-1/5 mx-5"></div>
+        <div className=" bg-red-600 h-56 w-1/5 mx-5"></div>
+        <div className=" bg-red-600 h-56 w-1/5 mx-5"></div>
+        <div className=" bg-red-600 h-56 w-1/5 mx-5"></div>
       </div>
     </div>
   );
