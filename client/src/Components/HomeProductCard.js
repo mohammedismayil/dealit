5c414feb64235bad04a5654c6d32ae9411180f85
mohammedismayil <mohammedismayil10@gmail.com>
2021-11-17 00:09:57 +0530

    adding initial UI changes

---
+++ client/src/Components/HomeProductCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function HomeProductCard() {
+  return (
+    <div className="w-1/4 border border-gray-300 shadow-sm rounded-md ml-10 mb-10 content-center justify-center flex flex-col">
+      <div>
+        <img
+          className="p-5"
+          src="https://apollo-singapore.akamaized.net/v1/files/xtd7smj45nv41-IN/image;s=272x0"
+        ></img>
+      </div>
+      <div className="flex">
+        <div className="bg-yellow-300 w-2 mr-5"></div>
+        <div>
+          <div className="text-lg font-strong">$13,333</div>
+          <div>Condition fresh laptop 8gb ram//500gbbhdd i5 laptop laptop</div>
+
+          <div className="flex flex-end">
+            <div>Indirapuram, Ghaziabad</div>
+            <div>Oct 20</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/src/Components/HomeProductLists.js
+++ client/src/Components/HomeProductLists.js
@@ -1,17 +1,10 @@
 import React from "react";
+import HomeProductCard from "./HomeProductCard";
 
 export default function HomeProductLists() {
   return (
-    <div className="flex content-center justify-center mx-20">
-      <div className="flex w-full bg-gray-600 flex-wrap m-auto content-center">
-        <div className=" bg-red-600 h-56 w-1/5 mx-5"></div>
-        <div className=" bg-red-600 h-56 w-1/5 mx-5"></div>
-        <div className=" bg-red-600 h-56 w-1/5 mx-5"></div>
-        <div className=" bg-red-600 h-56 w-1/5 mx-5"></div>
-        <div className=" bg-red-600 h-56 w-1/5 mx-5"></div>
-        <div className=" bg-red-600 h-56 w-1/5 mx-5"></div>
-        <div className=" bg-red-600 h-56 w-1/5 mx-5"></div>
-      </div>
+    <div className="flex">
+      <HomeProductCard></HomeProductCard>
     </div>
   );
 }
