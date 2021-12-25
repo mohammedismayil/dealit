1a43e41970485c633b01c8e34ed4118a4e1f4910
mohammedismayil <mohammedismayil10@gmail.com>
2021-12-25 21:37:14 +0530

    deployment commented for now

--- client/src/Components/HomeProductLists.js
+++ client/src/Components/HomeProductLists.js
@@ -3,7 +3,7 @@ import HomeProductCard from "./HomeProductCard";
 
 export default function HomeProductLists() {
   return (
-    <div className="flex">
+    <div className="flex pt-5">
       <HomeProductCard></HomeProductCard>
     </div>
   );
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -9,8 +9,10 @@ export default function Home() {
       <Header></Header>
       <Categories></Categories>
       <AutoAd></AutoAd>
-      <div className="font-bold text-lg">Fresh Recommendations</div>
-      <HomeProductLists></HomeProductLists>
+      <div className="container pl-10 pt-10 ">
+        <div className=" text-2xl ml-10">Fresh Recommendations</div>
+        <HomeProductLists></HomeProductLists>
+      </div>
     </div>
   );
 }
